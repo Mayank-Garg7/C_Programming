d80f8e71a8eaff91648f5efba8735a32c9318dce
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-23 13:44:19 +0530

    Update TicTacToe.c

--- Projects/TicTacToe.c
+++ Projects/TicTacToe.c
@@ -180,7 +180,7 @@ void rules()
     printf("\n1:Each player will be entering the number to put respective X or O in the desired position");
     printf("\n2:Player who gets a combination of 3 same characters either diagonal or horizontally or \n  vertically will be declared as the winner");
     printf("\n\nEnjoy the game! Be a Winner!\n\n");
-    printf("For more clarifications press Y else type any other character:- ");
+    printf("For more classifications press Y else type any other character:- ");
     scanf("%c",&link);
     if(link=='y' || link=='Y')
     {
