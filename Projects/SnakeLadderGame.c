f2ec0bc8478d977b708b5427bd8a2fc71d012709
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-11-28 22:12:09 +0530

    Update SnakeLadderGame.c

--- Projects/SnakeLadderGame.c
+++ Projects/SnakeLadderGame.c
@@ -1,15 +1,3 @@
-
-//C program for creating & simulating a Snake & Ladder Game
-//Constraints and Rules
-/*
-1.The game will begin with any dice value.
-2.If a 6(six) is appeared then a chance is awarded to that player.
-3.Only the location of current player is shown on the board,
-	the location of other player is mentioned below the board.
-4.Snakes:- 99 to  1, 65 to 40, 25 to 9.
-5.Ladder:- 70 to 93, 60 to 83, 13 to 42.
-
-*/
 #include<stdio.h>
 #include<stdlib.h>
 int rd()
